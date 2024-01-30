ffa4e7731748035c3b7a415e59497450cb4d2ca4
Celi003 <123420334+Celi003@users.noreply.github.com>
2024-01-30 22:16:49 +0100

    Début

---
+++ src/Parameter.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Seno4
+ */
+public class Parameter {
+
+        public static String nom = "root";
+        public static String pass = null;
+        public static String db = "jdbc:mysql://localhost:3306/gestion";
+        public static PreparedStatement stmt = null;     
+}
